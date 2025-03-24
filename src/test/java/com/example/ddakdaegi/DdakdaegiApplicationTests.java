ab3e7bafe7d614a910adc0df14562f2ad4efded6
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-03-24 16:56:05 +0900

    프로젝트 세팅

---
+++ src/test/java/com/example/ddakdaegi/DdakdaegiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddakdaegi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdakdaegiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
