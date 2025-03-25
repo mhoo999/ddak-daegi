f921c20d47aa8f5b9d8d60843b9de05369f6457d
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-03-25 16:26:27 +0900

    Fix : 반환 타입 수정
    ResponseEntity<T> -> Response<T>

--- src/main/java/com/example/ddakdaegi/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/ddakdaegi/domain/auth/controller/AuthController.java
@@ -8,11 +8,10 @@ import com.example.ddakdaegi.domain.auth.dto.request.LoginRequest;
 import com.example.ddakdaegi.domain.auth.dto.request.RegisterRequest;
 import com.example.ddakdaegi.domain.auth.dto.response.TokenDto;
 import com.example.ddakdaegi.domain.auth.service.AuthService;
-import com.example.ddakdaegi.global.common.response.SuccessResponse;
+import com.example.ddakdaegi.global.common.response.Response;
 import com.example.ddakdaegi.global.util.CookieUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,24 +26,24 @@ public class AuthController {
 	private final CookieUtil cookieUtil;
 
 	@PostMapping("/v1/auth/register")
-	public ResponseEntity<SuccessResponse<String>> signUp(@RequestBody RegisterRequest registerRequestDto,
+	public Response<String> signUp(@RequestBody RegisterRequest registerRequestDto,
 		HttpServletResponse response) {
 
 		TokenDto tokenDto = authService.signUp(registerRequestDto);
 		cookieUtil.addCookie(response, REFRESH_NAME, tokenDto.getRefreshToken(),
 			REFRESH_EXP_TIME);
 
-		return ResponseEntity.ok(new SuccessResponse<>(tokenDto.getAccessToken()));
+		return Response.of(tokenDto.getAccessToken());
 	}
 
 	@PostMapping("/v1/auth/login")
-	public ResponseEntity<SuccessResponse<String>> login(@RequestBody LoginRequest loginRequestDto,
+	public Response<String> login(@RequestBody LoginRequest loginRequestDto,
 		HttpServletResponse response) {
 
 		TokenDto tokenDto = authService.login(loginRequestDto);
 		cookieUtil.addCookie(response, REFRESH_NAME, tokenDto.getRefreshToken(),
 			REFRESH_EXP_TIME);
 
-		return ResponseEntity.ok(new SuccessResponse<>(tokenDto.getAccessToken()));
+		return Response.of(tokenDto.getAccessToken());
 	}
 }
