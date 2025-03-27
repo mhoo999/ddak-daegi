4e493fb349072347098ee5cc0a502628c2e35896
dereck-jun <yjpark0805@naver.com>
2025-03-27 15:18:13 +0900

    Refactor: 주문 생성과 개수 차감 로직 분리
    - 기존 두 로직이 붙어있어서 데드락이 자주 발생하게 되었기 때문에 수량을 바꾸는(`=update`) 부분을 `StockService`로 분리해서 `StockResponse`를 반환하도록 하고, 주문 생성 시 반환 값을 사용하도록 변경함
    - 테스트 코드에서 커넥션 풀의 최대 개수마다 재고가 1개씩 차감되던 부분이 동시에 커넥션을 얻어서 동시에 로직을 수행하고 나오게 돼서 발생하는 것으로 판단해서 `Thread.sleep`을 추가함
    
    Related to: #27

--- src/main/java/com/example/ddakdaegi/domain/order/service/OrderService.java
+++ src/main/java/com/example/ddakdaegi/domain/order/service/OrderService.java
@@ -2,18 +2,16 @@ package com.example.ddakdaegi.domain.order.service;
 
 import com.example.ddakdaegi.domain.member.entity.Member;
 import com.example.ddakdaegi.domain.member.repository.MemberRepository;
-import com.example.ddakdaegi.domain.order.dto.request.PromotionProductRequest;
 import com.example.ddakdaegi.domain.order.dto.response.OrderDetailResponse;
 import com.example.ddakdaegi.domain.order.dto.response.OrderResponse;
+import com.example.ddakdaegi.domain.order.dto.response.StockResponse;
 import com.example.ddakdaegi.domain.order.entity.Order;
 import com.example.ddakdaegi.domain.order.entity.OrderPromotionProduct;
 import com.example.ddakdaegi.domain.order.repository.OrderPromotionProductRepository;
 import com.example.ddakdaegi.domain.order.repository.OrderRepository;
 import com.example.ddakdaegi.domain.promotion.entity.PromotionProduct;
-import com.example.ddakdaegi.domain.promotion.repository.PromotionProductRepository;
 import com.example.ddakdaegi.global.common.dto.AuthUser;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,41 +25,23 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class OrderService {
 
-	private final PromotionProductRepository promotionProductRepository;
 	private final OrderRepository orderRepository;
 	private final OrderPromotionProductRepository orderPromotionProductRepository;
 	private final MemberRepository memberRepository;
 
 
 	@Transactional
-	public OrderResponse createOrder(AuthUser authUser, List<PromotionProductRequest> promotionProductRequests) {
-		List<Long> promotionProductIds = promotionProductRequests.stream()
-			.map(PromotionProductRequest::getPromotionProductId)
-			.collect(Collectors.toList());
+	public OrderResponse createOrder(AuthUser authUser, StockResponse stockResponse) {
 
-		Map<Long, Long> promotionProductIdToQuantityMap = promotionProductRequests.stream()
-			.collect(
-				Collectors.toMap(PromotionProductRequest::getPromotionProductId, PromotionProductRequest::getQuantity));
+		Member getMember = memberRepository.findById(authUser.getId())
+			.orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없음"));
 
-		List<PromotionProduct> promotionProducts = promotionProductRepository.findAllByIdIn(promotionProductIds);
-
-		long totalPrice = 0L;
-
-		for (PromotionProduct promotionProduct : promotionProducts) {
-			Long quantity = promotionProductIdToQuantityMap.get(promotionProduct.getId());
-			if (promotionProduct.getStock() < quantity) {
-				throw new RuntimeException("재고가 부족합니다.");
-			}
-			promotionProduct.decreaseStock(quantity);
-			totalPrice += quantity * promotionProduct.getPrice();
-		}
-
-		Order newOrder = Order.of(Member.fromAuthUser(authUser), totalPrice);
+		Order newOrder = Order.of(getMember, stockResponse.getTotalPrice());
 		orderRepository.save(newOrder);
 
-		List<OrderPromotionProduct> orderPromotionProducts = promotionProducts.stream()
+		List<OrderPromotionProduct> orderPromotionProducts = stockResponse.getPromotionProducts().stream()
 			.map(promotionProduct -> {
-				Long quantity = promotionProductIdToQuantityMap.get(promotionProduct.getId());
+				Long quantity = stockResponse.getPromotionProductIdToQuantityMap().get(promotionProduct.getId());
 				return OrderPromotionProduct.of(newOrder, promotionProduct, quantity);
 			}).collect(Collectors.toList());
 
