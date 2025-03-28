4ff673e9dca98623b9f245c33740c6acbc1d5502
myeonghoon <mhoo999@naver.com>
2025-03-28 11:14:50 +0900

    Test: 테스트 코드 추가

---
+++ src/test/java/com/example/ddakdaegi/domain/promotion/service/PromotionServiceTest.java
@@ -0,0 +1,54 @@
+package com.example.ddakdaegi.domain.promotion.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class PromotionServiceTest {
+
+	@Test
+	public void 재고보다_수량이_많을_경우_예외를_던진다() {
+		// given
+		// when
+		// then
+	}
+
+	@Test
+	public void 존재하지_않는_이미지_ID일_경우_예외를_던진다() {
+		// given
+		// when
+		// then
+	}
+
+	@Test
+	public void 존재하지_않는_제품_ID일_경우_예외를_던진다() {
+		// given
+		// when
+		// then
+	}
+
+	@Test
+	public void 시작_날짜가_종료_날짜_이후일_경우_예외를_던진다() {
+		// given
+		// when
+		// then
+	}
+
+	@Test
+	public void 존재하지_않는_프로모션_ID일_경우_예외를_던진다() {
+		// given
+		// when
+		// then
+	}
+
+	@Test
+	public void 잘못된_할인_정책일_경우_예외를_던진다() {
+		// given
+		// when
+		// then
+	}
+
+}
