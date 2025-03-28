31407b9f37945b55aa28badb407dbf0d9b90d7d7
yjn33 <yjn0032@gmail.com>
2025-03-28 10:52:49 +0900

    Refactor: ProductResponse.java 리팩토링

--- src/main/java/com/example/ddakdaegi/domain/image/dto/response/ImageResponse.java
+++ src/main/java/com/example/ddakdaegi/domain/image/dto/response/ImageResponse.java
@@ -1,5 +1,6 @@
 package com.example.ddakdaegi.domain.image.dto.response;
 
+import com.example.ddakdaegi.domain.image.entity.Image;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,4 +11,8 @@ public class ImageResponse {
 	private final Long imageId;
 	private final String imageUrl;
 	private final String imageName;
+
+	public static ImageResponse from(Image image) {
+		return new ImageResponse(image.getId(), image.getImageUrl(), image.getFileName());
+	}
 }
