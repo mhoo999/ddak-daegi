cccd837ea8b6bc33836e6840c29bef892bbe48c3
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-03-29 20:45:42 +0900

    Test : AdminMemberService 테스트코드작성

--- src/main/java/com/example/ddakdaegi/domain/member/dto/request/MemberUpdateRoleRequest.java
+++ src/main/java/com/example/ddakdaegi/domain/member/dto/request/MemberUpdateRoleRequest.java
@@ -1,8 +1,10 @@
 package com.example.ddakdaegi.domain.member.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class MemberUpdateRoleRequest {
 
 	private Long memberId;
